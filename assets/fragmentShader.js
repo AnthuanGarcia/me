ea266d7b12400147fa4d28ebe546bf589105f662
Anthuan <pinkyfloid0@gmail.com>
2023-01-26 23:31:14 -0600

    Other Shader

--- assets/fragmentShader.js
+++ assets/fragmentShader.js
@@ -12,46 +12,21 @@ void main() {
 export const FragmentShaders = [
 `
 uniform vec2 u_resolution;
-uniform float u_time;
 uniform vec2 u_mouse;
+uniform float u_time;
 
-#define _H 0.5
+#define ASPECT u_resolution.x / u_resolution.y
+
+#define SIZE 0.175
+#define RADIUS 0.05
+
+#define D2R 0.01745329
 
 mat2 rot2D(float angle) {
 
-    float c = cos(angle);
-    float s = sin(angle);
+	float s = sin(angle), c = cos(angle);
+	return mat2(c, -s, s, c);
 
-    return mat2(
-        c, -s,
-        s, c
-    );
-
-}
-
-vec3 rotate(vec3 p, vec3 a) {
-
-	p.yz *= rot2D(a.x);
-	p.xz *= rot2D(-a.y);
-	p.xy *= rot2D(a.z);
-
-	return p;
-
-}
-
-vec2 boxIntersection( in vec3 ro, in vec3 rd, vec3 boxSize, out vec3 outNormal ) {
-    vec3 m = 1.0/rd; // can precompute if traversing a set of aligned boxes
-    vec3 n = m*ro;   // can precompute if traversing a set of aligned boxes
-    vec3 k = abs(m)*boxSize;
-    vec3 t1 = -n - k;
-    vec3 t2 = -n + k;
-    float tN = max( max( t1.x, t1.y ), t1.z );
-    float tF = min( min( t2.x, t2.y ), t2.z );
-    if( tN>tF || tF<0.0) return vec2(-1.0); // no intersection
-    outNormal = (tN>0.0) ? step(vec3(tN),t1) : // ro ouside the box
-                           step(t2,vec3(tF));  // ro inside the box
-    outNormal *= -sign(rd);
-    return vec2( tN, tF );
 }
 
 float noise(vec2 st) {
@@ -79,75 +54,199 @@ float smoothNoise(vec2 st) {
 
 }
 
+float roundedboxIntersect( in vec3 ro, in vec3 rd, in vec3 size, in float rad )
+{
+    // bounding box
+    vec3 m = 1.0/rd;
+    vec3 n = m*ro;
+    vec3 k = abs(m)*(size+rad);
+    vec3 t1 = -n - k;
+    vec3 t2 = -n + k;
+    float tN = max( max( t1.x, t1.y ), t1.z );
+    float tF = min( min( t2.x, t2.y ), t2.z );
+    if( tN>tF || tF<0.0) return -1.0;
+    float t = tN;
+
+    // convert to first octant
+    vec3 pos = ro+t*rd;
+    vec3 s = sign(pos);
+    ro  *= s;
+    rd  *= s;
+    pos *= s;
+        
+    // faces
+    pos -= size;
+    pos = max( pos.xyz, pos.yzx );
+    if( min(min(pos.x,pos.y),pos.z) < 0.0 ) return t;
+
+    // some precomputation
+    vec3 oc = ro - size;
+    vec3 dd = rd*rd;
+    vec3 oo = oc*oc;
+    vec3 od = oc*rd;
+    float ra2 = rad*rad;
+
+    t = 1e20;        
+
+    // corner
+    {
+    float b = od.x + od.y + od.z;
+    float c = oo.x + oo.y + oo.z - ra2;
+    float h = b*b - c;
+    if( h>0.0 ) t = -b-sqrt(h);
+    }
+    // edge X
+    {
+    float a = dd.y + dd.z;
+    float b = od.y + od.z;
+    float c = oo.y + oo.z - ra2;
+    float h = b*b - a*c;
+    if( h>0.0 )
+    {
+        h = (-b-sqrt(h))/a;
+        if( h>0.0 && h<t && abs(ro.x+rd.x*h)<size.x ) t = h;
+    }
+    }
+    // edge Y
+    {
+    float a = dd.z + dd.x;
+    float b = od.z + od.x;
+    float c = oo.z + oo.x - ra2;
+    float h = b*b - a*c;
+    if( h>0.0 )
+    {
+        h = (-b-sqrt(h))/a;
+        if( h>0.0 && h<t && abs(ro.y+rd.y*h)<size.y ) t = h;
+    }
+    }
+    // edge Z
+    {
+    float a = dd.x + dd.y;
+    float b = od.x + od.y;
+    float c = oo.x + oo.y - ra2;
+    float h = b*b - a*c;
+    if( h>0.0 )
+    {
+        h = (-b-sqrt(h))/a;
+        if( h>0.0 && h<t && abs(ro.z+rd.z*h)<size.z ) t = h;
+    }
+    }
+
+    if( t>1e19 ) t=-1.0;
+    
+    return t;
+}
+
+vec3 roundedboxNormal( in vec3 pos, in vec3 siz, in float rad )
+{
+    return sign(pos)*normalize(max(abs(pos)-siz,0.0));
+}
+
+vec3 sun = vec3(50, 100, -50);
+
+vec3 hsv2rgb(vec3 c) {
+
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+
+}
+
+vec3 shade(vec3 p, vec3 ro) {
+
+	vec3 n = roundedboxNormal(p, vec3(SIZE), RADIUS);
+
+	vec3 lightDir = normalize(sun - p);
+	vec3 viewDir  = normalize(ro - p);
+	vec3 reflLight = normalize(reflect(-lightDir, n));
+
+	float diff = max(dot(n, lightDir), 0.0);
+	float spec = pow(max(dot(reflLight, viewDir), 0.0), 64.0);
+	float rim  = 1.0 - dot(viewDir, n);
+
+	//vec3 ambient = vec3(0.5, 0.0, 0.25);
+	//vec3 diffuse = vec3(1.0, 0.0, 0.5);
+
+	//vec3 ambient = hsv2rgb(
+	//	vec3(0.975)
+	//);
+
+	vec3 cc = hsv2rgb(vec3(0.45));
+	vec3 cv = hsv2rgb(vec3(0.975));
+	float kw = (dot(n, lightDir) + 1.0) * 0.5; 
+
+	vec3 ambient = mix(cc, cv, kw);
+
+	vec3 diffuse = vec3(0.75);
+	vec3 rimLight = vec3(0.8);
+
+	return clamp(
+		ambient +
+		diff * diffuse +
+		(1.0 - diff) * rim  * rimLight +
+		spec,
+		0.0, 1.0
+	);
+}
+
+#define SPEED u_time * 0.25
+
+vec2 brickTile(vec2 _st, float _zoom) {
+
+    _st *= _zoom;
+
+    vec2 even = mod(_st, 2.0);
+    vec2 move = sign(even - 1.0) * SPEED;
+
+    _st.y += move.x * step(1.0,   mod(SPEED, 2.0));
+    _st.x += move.y * step(-1.0, -mod(SPEED, 2.0));
+
+    return fract(_st) - 0.5;
+
+}
+
 void main() {
 
-	vec2 uv = 2.0*(gl_FragCoord.xy / u_resolution) - 1.0;
-	uv.x *= u_resolution.x / u_resolution.y;
+	vec2 uv = gl_FragCoord.xy / u_resolution;
+	uv.x *= ASPECT;
 
-	vec2 mou = 2.0*(u_mouse / u_resolution) - 1.0;
-	mou.x *= u_resolution.x / u_resolution.y * -1.0;
+	vec3 col = vec3(0.9725, 0.8784, 0.9451);
 
-	vec3 col = mix(
-		vec3(0.9373, 0.7765, 0.9843),
-		vec3(0.9765, 0.7686, 0.8784),
-		uv.y * 0.5 + 0.5
-	);
+	float noi = smoothNoise(uv*8.0 + u_time*0.25) * 0.1;
 
-	uv *=  3.0;
-	mou *= 3.0;
+	uv = brickTile(uv, 2.25);
+	uv *= rot2D(-5.0 * D2R);
 
-	vec2 fuv = fract(uv) - 0.5;
-	vec2 iuv = floor(uv) - 0.5;
-	vec2 imo = floor(-mou) - 0.5;
+	//uv.y += u_time*0.05*sign(mod(floor(uv.x), 2.0) - 0.5);
 
-	vec3 ro = vec3(0, 0, -1.5);
-	vec3 rd = normalize(vec3(fuv, 1.0));
+	//float sy = sign(mod(floor(uv.y), 2.0) - 0.5);
+	float sy = sign(floor(uv.x) - 0.5);
 
-	if (iuv == imo) {
+	//uv = fract(uv) - 0.5;
 
-		ro = rotate(ro, vec3(sin(3.0*u_time)));
-		rd = rotate(rd, vec3(sin(3.0*u_time)));
+	vec3 ro = vec3(uv, -2.0);
+	vec3 rd = vec3(0, 0, 1);
 
-	}
+	mat2 rTime = rot2D(u_time * sy * 0.5);
+	mat2 r30   = rot2D(30.0 * D2R);
 
-	float r = 0.2 + 0.1*smoothNoise(uv*4.0 + vec2(u_time));
+	ro.yz *= r30;
+	rd.yz *= r30;
 
-	vec3 n;
-	vec2 box = boxIntersection(ro, rd, vec3(r), n);
+	ro.xz  *= rTime;
+	rd.xz  *= rTime;
+	sun.xz *= rTime;
 
-	if (box.x > 0.0) {
+	float t = roundedboxIntersect(ro, rd, vec3(SIZE), RADIUS + noi);
 
-		vec3 sun = vec3(50.0, 100, -50.0);
-		vec3 pos = ro + rd*box.x;
+	if (t > 0.0) {
 
-		vec3 lightDir = normalize(sun - pos);
-
-		float diff = max( dot(lightDir, n), 0.0 );
-
-			//rim = pow(rim, 1.5);
-
-		vec3 col1 = vec3(0.0, 0.0, 0.0),
-		col2 = vec3(0.9529, 0.7412, 0.3216),
-		col3 = vec3(0.9608, 0.5059, 0.8549);
-
-		float dir = (1.0 + diff) * 0.5;
-
-        vec3 colBase = mix(
-            mix(col1, col2, dir/_H),
-            mix(col2, col3, (dir - _H)/(1.0 - _H)),
-            step(_H, dir)
-        );
-
-		vec3 finalCol = clamp(
-			colBase,
-			0.0, 1.0
-		);
-
-		col = finalCol;
+		vec3 p = ro + rd*t;
+		col = shade(p, ro);
 
 	}
 
-	gl_FragColor = vec4(col * sqrt(col), 1);
+	gl_FragColor = vec4(col, 1);
 
 }`,
 `
