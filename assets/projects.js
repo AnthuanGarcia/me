1f6a68eb22634969df1902057b8cdbff71b2cf9b
Anthuan Garcia <pinky.floid0@gmail.com>
2023-08-26 18:45:35 -0600

    Minor corrections and proyects added

--- assets/projects.js
+++ assets/projects.js
@@ -8,14 +8,14 @@ export const Projects = [
 		],
 		"desc": "El proyecto estaba enfocado principalmente en la domotica, la idea era crear una base de datos de controles remotos para manipular multitud de dispositivos del hogar desde una sola aplicación, la app también ofrecía funcionalidades de programar fechas de encendido para cualquier dispositivo, y una lista de canales favoritos.",
 		"imgs": [
-			"https://i.ibb.co/R2zRzTb/Screenshot-20210502-180010.jpg",
-			"https://i.ibb.co/4Zh3KRX/Screenshot-20210502-180149.jpg",
-			"https://i.ibb.co/qYYfwkQ/Screenshot-20210502-180103.jpg",
-			"https://i.ibb.co/KDgnyp6/Screenshot-20210502-180024.jpg",
-			"https://i.ibb.co/qr1cyzH/Screenshot-20210502-180159.jpg",
-			"https://i.ibb.co/XsCGq7z/Screenshot-20210502-180153.jpg",
-			"https://i.ibb.co/Br4D6TZ/Screenshot-20210502-180107.jpg",
-			"https://i.ibb.co/kmqgrzt/Screenshot-20210502-180115.jpg"
+			"../assets/img/arduremote/s1.jpg",
+			"../assets/img/arduremote/s2.jpg",
+			"../assets/img/arduremote/s3.jpg",
+			"../assets/img/arduremote/s4.jpg",
+			"../assets/img/arduremote/s5.jpg",
+			"../assets/img/arduremote/s6.jpg",
+			"../assets/img/arduremote/s7.jpg",
+			"../assets/img/arduremote/s8.jpg"
 		],
 		"mobil": true
 	},
@@ -28,12 +28,12 @@ export const Projects = [
 		],
 		"desc": "Este fue mi primer proyecto de cierta manera, era simplemente una página web para realizar cotizaciones de manera simple a través de un formulario y poder visualizar todas las cotizaciones hechas hasta el momento, yo me encargue de todo el lado del servidor en Node.js.",
 		"imgs": [
-			"https://i.ibb.co/Tbk1fpB/Captura-de-pantalla-2.png",
-			"https://i.ibb.co/GMdG0J1/Captura-de-pantalla-3.png",
-			"https://i.ibb.co/y4DkhL8/Captura-de-pantalla-4.png",
-			"https://i.ibb.co/TRHx2JF/Captura-de-pantalla-5.png",
-			"https://i.ibb.co/txqRbFZ/Captura-de-pantalla-7.png",
-			"https://i.ibb.co/KFQ0FDw/Captura-de-pantalla-8.png"
+			"../assets/img/imch/s1.png",
+			"../assets/img/imch/s2.png",
+			"../assets/img/imch/s3.png",
+			"../assets/img/imch/s4.png",
+			"../assets/img/imch/s5.png",
+			"../assets/img/imch/s6.png"
 		],
 		"mobil": false
 	},
@@ -45,10 +45,10 @@ export const Projects = [
 		],
 		"desc": "Este proyecto consistía en la implementación de cámaras a través de un microcontrolador conectado a un servidor para enviar la transmisión en tiempo real hacia cualquier dispositivo con la aplicación instalada. El objetivo principal era el de crear cámaras de bajo costo.",
 		"imgs": [
-			"https://i.ibb.co/k1jhKrb/Screenshot-20210809-104949.jpg",
-			"https://i.ibb.co/JsRY9GT/Screenshot-20210809-104952.jpg",
-			"https://i.ibb.co/fnLcdq5/Screenshot-20210809-105011.jpg",
-			"https://i.ibb.co/QfV7nwr/Screenshot-20210809-105004.jpg"
+			"../assets/img/espcam/s1.jpg",
+			"../assets/img/espcam/s2.jpg",
+			"../assets/img/espcam/s3.jpg",
+			"../assets/img/espcam/s4.jpg"
 		],
 		"mobil": true
 	},
@@ -60,13 +60,43 @@ export const Projects = [
 		],
 		"desc": "Bag Guy es mi primer videojuego, es un plataformas sencillo basado en un personaje con una bolsa de papel en la cabeza, el juego principalmente lo desarrolle para probar de primera el desarrollo de videojuegos, actualmente el juego esta publicado en Itch.io.",
 		"imgs": [
-			"https://i.ibb.co/Mn0Msww/Captura-de-pantalla-35.png",
-			"https://i.ibb.co/8YDWCk6/Captura-de-pantalla-37.png",
-			"https://i.ibb.co/cQrkvG5/Captura-de-pantalla-38.png",
-			"https://i.ibb.co/tQBn0YC/Captura-de-pantalla-41.png",
-			"https://i.ibb.co/6vwVpYK/Captura-de-pantalla-43.png",
-			"https://i.ibb.co/FB2QdQD/Captura-de-pantalla-47.png",
-			"https://i.ibb.co/p4FjvH5/Captura-de-pantalla-49.png"
+			"../assets/img/bagguy/s1.png",
+			"../assets/img/bagguy/s2.png",
+			"../assets/img/bagguy/s3.png",
+			"../assets/img/bagguy/s4.png",
+			"../assets/img/bagguy/s5.png",
+			"../assets/img/bagguy/s6.png",
+			"../assets/img/bagguy/s7.png"
+		],
+		"mobil": false
+	},
+	{
+		"title": "Ambient Monitor",
+		"langs": [
+			"Flutter",
+			"C/C++"
+		],
+		"desc": "El proyecto consiste en la captura de datos en tiempo real para la monitorización de cualquier habitación con acceso a internet, a través de un sistema Plug & Use que se conecta de manera automática a una aplicación móvil desarrollada en flutter.",
+		"imgs": [
+			"../assets/img/ambientMonitor/a1.png",
+			"../assets/img/ambientMonitor/a2.png",
+			"../assets/img/ambientMonitor/a3.png",
+			"../assets/img/ambientMonitor/r1.png",
+			"../assets/img/ambientMonitor/r2.png"
+		],
+		"mobil": true
+	},
+	{
+		"title": "U",
+		"langs": [
+			"C#",
+			"Unity"
+		],
+		"desc": "El prototipo de un prototipo de un juego basado en grafos.",
+		"imgs": [
+			"../assets/img/u/s1.png",
+			"../assets/img/u/s2.png",
+			"../assets/img/u/playu.gif"
 		],
 		"mobil": false
 	}
