44463d211ebd6e25ad91b528797c2e0442accda6
Anthuan Garcia <pinky.floid0@gmail.com>
2023-10-17 23:24:38 -0600

    SmoothNoise func changed for incompatibility problems

--- assets/fragmentShader.js
+++ assets/fragmentShader.js
@@ -24,28 +24,16 @@ mat2 rot2D(float angle) {
 
 }
 
-float noise(vec2 st) {
-
-    return fract(sin( dot( st.xy, vec2(12.9898,78.233) ) ) * 43758.5453123);
-
+float noise(vec2 p) {
+    return fract(sin(p.x * 10.0 + p.y * 1234.5) * 5647.0);
 }
 
-float smoothNoise(vec2 st) {
+float smoothNoise(vec2 n) {
 
-    vec2 ipos = floor(st);
-    vec2 fpos = fract(st);
-
-    fpos = fpos*fpos * (3.0 - 2.0 * fpos);
-
-    float bl = noise(ipos);
-    float br = noise(ipos + vec2(1, 0));
-    float b  = mix(bl, br, fpos.x);
-    
-    float tl = noise(ipos + vec2(0, 1));
-    float tr = noise(ipos + vec2(1));
-    float t  = mix(tl, tr, fpos.x);
-
-    return mix(b, t, fpos.y);
+    const vec2 d = vec2(0.0, 1.0);
+    vec2 b = floor(n);
+    vec2 f = smoothstep(vec2(0.0), vec2(1.0), fract(n));
+    return mix(mix(noise(b), noise(b + d.yx), f.x), mix(noise(b + d.xy), noise(b + d.yy), f.x), f.y);
 
 }
 
@@ -249,28 +237,16 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
-float noise(vec2 st) {
-
-    return fract(sin( dot( st.xy, vec2(12.9898,78.233) ) ) * 43758.5453123);
-
+float noise(vec2 p) {
+    return fract(sin(p.x * 10.0 + p.y * 1234.5) * 5647.0);
 }
 
-float smoothNoise(vec2 st) {
+float smoothNoise(vec2 n) {
 
-    vec2 ipos = floor(st);
-    vec2 fpos = fract(st);
-
-    fpos = fpos*fpos * (3.0 - 2.0 * fpos);
-
-    float bl = noise(ipos);
-    float br = noise(ipos + vec2(1, 0));
-    float b  = mix(bl, br, fpos.x);
-    
-    float tl = noise(ipos + vec2(0, 1));
-    float tr = noise(ipos + vec2(1));
-    float t  = mix(tl, tr, fpos.x);
-
-    return mix(b, t, fpos.y);
+    const vec2 d = vec2(0.0, 1.0);
+    vec2 b = floor(n);
+    vec2 f = smoothstep(vec2(0.0), vec2(1.0), fract(n));
+    return mix(mix(noise(b), noise(b + d.yx), f.x), mix(noise(b + d.xy), noise(b + d.yy), f.x), f.y);
 
 }
 
@@ -395,28 +371,16 @@ vec2 boxIntersection( in vec3 ro, in vec3 rd, vec3 boxSize, out vec3 outNormal )
     return vec2( tN, tF );
 }
 
-float noise(vec2 st) {
-
-    return fract(sin( dot( st.xy, vec2(12.9898,78.233) ) ) * 43758.5453123);
-
+float noise(vec2 p) {
+    return fract(sin(p.x * 10.0 + p.y * 1234.5) * 5647.0);
 }
 
-float smoothNoise(vec2 st) {
+float smoothNoise(vec2 n) {
 
-    vec2 ipos = floor(st);
-    vec2 fpos = fract(st);
-
-    fpos = fpos*fpos * (3.0 - 2.0 * fpos);
-
-    float bl = noise(ipos);
-    float br = noise(ipos + vec2(1, 0));
-    float b  = mix(bl, br, fpos.x);
-    
-    float tl = noise(ipos + vec2(0, 1));
-    float tr = noise(ipos + vec2(1));
-    float t  = mix(tl, tr, fpos.x);
-
-    return mix(b, t, fpos.y);
+    const vec2 d = vec2(0.0, 1.0);
+    vec2 b = floor(n);
+    vec2 f = smoothstep(vec2(0.0), vec2(1.0), fract(n));
+    return mix(mix(noise(b), noise(b + d.yx), f.x), mix(noise(b + d.xy), noise(b + d.yy), f.x), f.y);
 
 }
 
@@ -519,28 +483,16 @@ vec2 sphIntersect( in vec3 ro, in vec3 rd, in vec3 ce, float ra )
     return vec2( -b-h, -b+h );
 }
 
-float noise(vec2 st) {
-
-    return fract(sin( dot( st.xy, vec2(12.9898,78.233) ) ) * 43758.5453123);
-
+float noise(vec2 p) {
+    return fract(sin(p.x * 10.0 + p.y * 1234.5) * 5647.0);
 }
 
-float smoothNoise(vec2 st) {
+float smoothNoise(vec2 n) {
 
-    vec2 ipos = floor(st);
-    vec2 fpos = fract(st);
-
-    fpos = fpos*fpos * (3.0 - 2.0 * fpos);
-
-    float bl = noise(ipos);
-    float br = noise(ipos + vec2(1, 0));
-    float b  = mix(bl, br, fpos.x);
-    
-    float tl = noise(ipos + vec2(0, 1));
-    float tr = noise(ipos + vec2(1));
-    float t  = mix(tl, tr, fpos.x);
-
-    return mix(b, t, fpos.y);
+    const vec2 d = vec2(0.0, 1.0);
+    vec2 b = floor(n);
+    vec2 f = smoothstep(vec2(0.0), vec2(1.0), fract(n));
+    return mix(mix(noise(b), noise(b + d.yx), f.x), mix(noise(b + d.xy), noise(b + d.yy), f.x), f.y);
 
 }
 
